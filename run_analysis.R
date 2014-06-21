2410cb03a739c4588335862087f23b662e763c96
gohks <gohkaisong@gmail.com>
2014-06-21 18:49:15 +0800

    This is the script that will combine the train and test data and subset the required data

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+setwd("C:/MOOC/Getting and Cleaning Data/Project")
+
+# Read feature labels
+features <- read.table(normalizePath(file.path(".","UCI HAR Dataset","features.txt")))
+
+# Read activity labels
+activities <- read.table(normalizePath(file.path(".","UCI HAR Dataset","activity_labels.txt")))
+
+testdir <- file.path(".","UCI HAR Dataset","test")
+traindir <- file.path(".","UCI HAR Dataset","train")
+
+# Initialize the holder data frame
+holder <- NULL
+
+for (i in 2:length(dir(testdir))){
+	testfilename <- file.path(testdir,dir(testdir)[i])
+ 	testdata <- read.table(normalizePath(testfilename))
+ 	trainfilename <- file.path(traindir,dir(traindir)[i])
+ 	traindata <- read.table(normalizePath(trainfilename))
+	if(i==2) 
+		holder <- rbind(testdata, traindata)
+	else
+ 		holder <- cbind(holder,rbind(testdata, traindata))
+}
+
+# Rename the columns in the data frame
+
+names(holder)[1] <- "subject"
+names(holder)[ncol(holder)] <- "activity"
+
+for(i in 2:(ncol(holder)-1)){
+	names(holder)[i] <- toString(features[(i-1),2])
+}
+
+# Recode the activities 
+for(i in 1:nrow(activities)){
+   code <- activities[i,1]
+   name <- toString(activities[i,2])
+   holder$activity <- replace(holder$activity, holder$activity == code,name) 
+}
+
+
+final <- holder[ , grepl("subject|mean\\()|std\\()|activity" , names(holder))]
+write.table(final, "tidydata.txt")
+
+
